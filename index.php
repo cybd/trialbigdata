d8d90969b296d83c14adb174a974dc4e67f8f1e5
Sergei Matros <cybd@bigmir.net>
2016-02-21 16:22:47 +0000

    Skip header rows. Fixed insert.

--- index.php
+++ index.php
@@ -37,18 +37,6 @@ if (!($stmt = $mysqli->prepare($insertQuery))) {
     echo 'Prepare failed: ' . $mysqli->errno . ' ' . $mysqli->error;
 }
 
-// $mysqli->close();
-
-/*$rowTypeOneColumns = [
-    'id','login','phone','age','hobby','category','position','address','date','site','company','card','email','name','color'
-];
-$rowTypeTwoColumns = [
-    'id','login','phone','age','post','ra','hobby','position','address','date','site','company','card','email','name','color'
-];
-$rowTypeThreeColumns = [
-    'name','email','birthdate','company','icard','orgnum','phone','phone2','address','city','zip','region','hobby','dep','sex'
-];*/
-
 /*
   columnName => [rowtype1, rowtype2, rowtype3,]
 */
@@ -66,8 +54,7 @@ $rowMapping = [
     'sex'         => [null, null, 14],
 ];
 
-// $inputFilename = 'test_data.csv';
-$inputFilename = 'my_test_data.csv';
+$inputFilename = 'test_data.csv';
 
 $fileHandle = fopen($inputFilename, 'r');
 
@@ -75,31 +62,47 @@ if (!$fileHandle) {
     die('Have problem with opening file ' . $inputFilename);
 }
 
+// use prepared statements for multiple inserts
+if (!($stmt->bind_param(
+    'sssssssssss',
+    $name,
+    $phone,
+    $email,
+    $address,
+    $company,
+    $position,
+    $hobby,
+    $card,
+    $birthdate,
+    $site,
+    $sex
+))) {
+    echo 'Bind failed: ' . $mysqli->errno . ' ' . $mysqli->error;
+}
+
 $count = 0;
-$rowType = null;
-/* TODO: skip insert into db header rows for each rowtype */
 while (($lineParts = fgetcsv($fileHandle, 4096, ',')) !== false) {
 
     // var_dump($lineParts);
 
-    $rowType = isset($rowType) ? $rowType : -1;
+    $rowType = isset($rowType) ? $rowType : null;
     $keyPart = $lineParts[4];
 
     switch ($keyPart) {
         case 'hobby':
             // set row type 1
             $rowType = 0;
-            break;
+            continue 2;
 
         case 'post':
             // set row type 2
             $rowType = 1;
-            break;
+            continue 2;
 
         case 'icard':
             // set row type 3
             $rowType = 2;
-            break;
+            continue 2;
     }
 
     // var_dump($rowType);
@@ -114,58 +117,10 @@ while (($lineParts = fgetcsv($fileHandle, 4096, ',')) !== false) {
         }
     }
 
-/*    $columnNamesArr = [
-        'name',
-        'phone',
-        'email',
-        'address',
-        'company',
-        'position',
-        'hobby',
-        'card',
-        'birthdate',
-        'site',
-        'sex',
-    ];
-
-    print_r($columnNamesArr);
-
-    var_dump(
-        $name,
-        $phone,
-        $email,
-        $address,
-        $company,
-        $position,
-        $hobby,
-        $card,
-        $birthdate,
-        $site,
-        $sex
-    );*/
-
-    if ($count > 10) {
+/*    if ($count > 10) {
         break;
-    }
+    }*/
     $count++;
-
-    // use prepared statements for multiple inserts
-    if (!($stmt->bind_param(
-        'sssssssssss',
-        $name,
-        $phone,
-        $email,
-        $address,
-        $company,
-        $position,
-        $hobby,
-        $card,
-        $birthdate,
-        $site,
-        $sex
-    ))) {
-        echo 'Bind failed: ' . $mysqli->errno . ' ' . $mysqli->error;
-    }
 
     if (!$stmt->execute()) {
         echo 'Execute failed: ' . $mysqli->errno . ' ' . $mysqli->error;
